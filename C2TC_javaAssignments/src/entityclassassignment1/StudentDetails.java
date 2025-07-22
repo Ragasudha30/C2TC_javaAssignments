7bdc896d8625bae4cc676f8cce1b31315fd3cdb2
HP <HP@Sudha>
2025-07-22 13:46:50 +0530

    First Assignment

---
+++ C2TC_javaAssignments/src/entityclassassignment1/StudentDetails.java
@@ -0,0 +1,18 @@
+package entityclassassignment1;
+
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		
+		        Student s1 = new Student("Arun", 101, "IT");
+		        Student s2 = new Student("Beena", 102, "CSE");
+		        Student s3 = new Student("Charan", 103, "ECE");
+
+		        System.out.println("Student Details:");
+		        s1.display();
+		        s2.display();
+		        s3.display();
+
+	}
+
+}
